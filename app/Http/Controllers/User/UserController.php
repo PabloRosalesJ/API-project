7317f7db64d85ca48277a16ea3e108fdebd93d6e
PabloRosalesJ <rosalesjuarezpablojesus@gmail.com>
2021-05-10 00:10:11 -0500

    ResourceController for model User

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,17 +16,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $users = User::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json(['data' => $users], 200);
     }
 
     /**
@@ -35,51 +29,101 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6|confirmed',
+            ]
+        );
+
+        $data['password'] = Hash::make($data['password']);
+        $data['verified'] = User::USER_NOT_VERIFIED;
+        $data['verification_token'] = User::generateVerificationToken();
+        $data['admin'] = User::USER_NOT_VERIFIED;
+
+        $user = User::create($data);
+
+        return response()->json(['data' => $user], 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  User  $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return response()->json(['data' => $user], 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $data = $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email,'.$user->id,
+                'password' => 'required|min:6|confirmed',
+                'admin' => 'in:' . User::USER_ADMIN . ',' . User::USER_NOT_ADMIN,
+            ]
+        );
+
+        $user->name = $data['name'];
+
+        if ($request->has('email') && $request->email != $user->email) {
+            $user->verified = User::USER_NOT_VERIFIED;
+            $user->verification_token = User::generateVerificationToken();
+        }
+
+        if ($request->has('password')) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        if ($request->has('admin')) {
+            if (!$user->isVerified()) {
+                return response()->json(
+                    [
+                        'error' => 'Se requiere ser usuario veriificado para cambiar estado de administración',
+                        'code' => 409
+                    ],
+                    409
+                );
+            }
+        }
+
+        if (!$user->isDirty()) {
+            return response()->json(
+                [
+                    'error' => 'Especifica almenos un campo para actualizar',
+                    'code' => 422
+                ],
+                422
+            );
+        }
+
+        $user->update($data);
+
+        return response()->json(['data' => $user], 200);
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['data' => $user], 204);
     }
 }
